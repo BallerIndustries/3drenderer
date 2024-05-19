2f464e6979092080e6442414278715e8b4eed642
Angus Cheng <baller.industries@hotmail.com>
2024-05-19 11:19:47 +0800

    world matrix

--- src/main.c
+++ src/main.c
@@ -120,11 +120,10 @@ void update(void) {
 
     // Change the scale/rotation values per animation frame
     mesh.rotation.x += 0.01;
-    // mesh.rotation.y += 0.01;
-    // mesh.rotation.z += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 
-    // mesh.translation.x += 0.01;
-    
+    mesh.translation.x += 0.01;
     mesh.translation.z = 5.0;
 
     // Create a scale, rotation and translation matrices that will be used to mulitply the mesh vertices
@@ -150,11 +149,16 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-            transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
-            transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
+            // Create a world matrix combining scale, rotation and translation matrices
+            mat4_t world_matrix = mat4_identity(); 
+            world_matrix = mat4_mul_mat4(scale_matrix, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_x, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_y, world_matrix);
+            world_matrix = mat4_mul_mat4(rotation_matrix_z, world_matrix);
+            world_matrix = mat4_mul_mat4(translation_matrix, world_matrix);
+
+            // Multiply the world matrix by the original vector
+            transformed_vertex = mat4_mul_vec4(world_matrix, transformed_vertex);
 
             // Save transformed vertex
             transformed_vertices[j] = transformed_vertex;
