a19761c0b8579a4c80a4c54f2ea3bf09a7fa06c0
Angus Cheng <baller.industries@hotmail.com>
2024-05-19 10:26:57 +0800

    rotation

--- src/main.c
+++ src/main.c
@@ -120,16 +120,19 @@ void update(void) {
 
     // Change the scale/rotation values per animation frame
     mesh.rotation.x += 0.01;
-    mesh.rotation.y += 0.01;
-    mesh.rotation.z += 0.01;
-    // mesh.scale.x += 0.002;
-    // mesh.scale.y += 0.001; 
-    mesh.translation.x += 0.01;
+    // mesh.rotation.y += 0.01;
+    // mesh.rotation.z += 0.01;
+
+    // mesh.translation.x += 0.01;
+    
     mesh.translation.z = 5.0;
 
-    // Create a scale matrix that will be used to mulitply the mesh vertices
+    // Create a scale, rotation and translation matrices that will be used to mulitply the mesh vertices
     mat4_t scale_matrix = mat4_make_scale(mesh.scale.x, mesh.scale.y, mesh.scale.y);
     mat4_t translation_matrix = mat4_make_translation(mesh.translation.x, mesh.translation.y, mesh.translation.z);
+    mat4_t rotation_matrix_x = mat4_make_rotation_x(mesh.rotation.x);
+    mat4_t rotation_matrix_y = mat4_make_rotation_y(mesh.rotation.y);
+    mat4_t rotation_matrix_z = mat4_make_rotation_z(mesh.rotation.z);
 
     int num_faces = array_length(mesh.faces); 
 
@@ -147,9 +150,10 @@ void update(void) {
         for (int j = 0; j < 3; j++) {
             vec4_t transformed_vertex = vec4_from_vec3(face_vertices[j]);
 
-            // TODO: Use a matrix to scale our original vertex
-            // TODO: Multiply the scale_matrix by the vertex
             transformed_vertex = mat4_mul_vec4(scale_matrix, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_x, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_y, transformed_vertex);
+            transformed_vertex = mat4_mul_vec4(rotation_matrix_z, transformed_vertex);
             transformed_vertex = mat4_mul_vec4(translation_matrix, transformed_vertex);
 
             // Save transformed vertex
